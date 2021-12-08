ab3dfada9231874cfb690d2a2800b2355905291d
Masroor <19424169+masrooor@users.noreply.github.com>
2021-12-08 18:52:32 +0500

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const Members = [
+    {
+        id: 1, 
+        name: "Masroor",
+        email : 'masroor@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2, 
+        name: "Ahmed",
+        email : 'ahmed@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3, 
+        name: "Shaan",
+        email : 'shaan@gmail.com',
+        status: 'active'
+    },
+    
+];
+module.exports = Members;
